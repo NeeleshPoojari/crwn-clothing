13e3558bf24b1072535b65155e2337ae2d6906f6
Neelesh Poojari <npoojari@sapient.com>
2019-11-26 17:44:06 +0530

    updating app with shop page, adding collection preview and collection item component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import HomePage from "./pages/homepage/homepage.component";
-import "./App.css";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-const HatsPage = props => {
-  console.log(props);
-  return <h1>HATS</h1>;
-};
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
+import "./App.css";
 
 function App() {
   return (
     <div>
     <Switch>
       <Route exact path="/" component={HomePage} />
-      <Route path="/hats" component={HatsPage} />
+      <Route path="/shop" component={ShopPage} />
     </Switch>
     </div>
   );

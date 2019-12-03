58dc4e5128281baf21647df53c38fc1c7996bc33
Neelesh Poojari <npoojari@sapient.com>
2019-12-03 19:15:10 +0530

    Added collection overview component + modified shoppage

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,25 +1,11 @@
 import React from "react";
-import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+import CollectionsOverview from '../../components/collections-overview/collection-overview.components';
 
-import SHOP_DATA from "./shop.data";
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return <div className='shop-page'>
-      {collections.map(({id, ...otherCollectionProps }) => (
-        <CollectionPreview key={id} {...otherCollectionProps}></CollectionPreview>
-      ))}
-    </div>;
-  }
-}
-
+const ShopPage = () =>(
+  <div className='shop-page'>
+     <CollectionsOverview />
+    </div>
+);
+  
 export default ShopPage;

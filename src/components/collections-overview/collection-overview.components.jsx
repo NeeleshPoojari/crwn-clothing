5b04ab67b5bfab01ea9f0cd196f82bbc599093e4
Neelesh Poojari <npoojari@sapient.com>
2019-12-04 16:31:22 +0530

    Moved directory and shop data into its own reducers, created resp selectors and updated corresp components with new redux flow for directory/shop

--- src/components/collections-overview/collection-overview.components.jsx
+++ src/components/collections-overview/collection-overview.components.jsx
@@ -3,20 +3,23 @@ import { connect } from 'react-redux';
 
 import CollectionPreview from '../collection-preview/collection-preview.component';
 
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
 import './collections-overview.styles.scss';
 
-const CollectionsOverview = ({ collections }) => (
+const CollectionsOverview = ({ collections }) => {
+  console.log("[[[collections",collections);
+  return (
   <div className='collections-overview'>
     {collections.map(({ id, ...otherCollectionProps }) => (
       <CollectionPreview key={id} {...otherCollectionProps} />
     ))}
   </div>
-);
+)}
+;
 
 const mapStateToProps = (state) => ({
-  collections: selectCollections(state)
+  collections: selectCollectionsForPreview(state)
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../components/collections-overview/collection-overview.components';
+import CollectionPage from '../collection/collection.component';
 
 
-const ShopPage = () =>(
+const ShopPage = ({ match }) => {
+  return (
   <div className='shop-page'>
-     <CollectionsOverview />
-    </div>
-);
+     <Route exact path={`${ match.path }`} component={CollectionsOverview} />
+     <Route path={`${ match.path }/:collectionId`} component={CollectionPage}/>
+  </div>
+)};
   
 export default ShopPage;
